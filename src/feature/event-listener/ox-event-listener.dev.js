39b42a1875000b6a66a7c58842e2043056c49c3a
Ian Maurmann <ikmxx@yahoo.com>
2022-11-26 13:18:21 -0700

    Add JsDoc to Event Listener

--- src/feature/event-listener/ox-event-listener.dev.js
+++ src/feature/event-listener/ox-event-listener.dev.js
@@ -3,13 +3,22 @@ if( typeof Ox === 'undefined' || Ox === null || Ox === false){
     var Ox = {};
 }
 
+
+
 // Check if Ox Event Listener exists
 if( typeof Ox.EventListener === 'undefined' || Ox.EventListener === null || Ox.EventListener === false){
     Ox.EventListener = {};
 }
 
 
-// Delegate Event
+
+/**
+ * Delegate a UI Event
+ * @author Ian Maurmann
+ * @param {string} selector - jQuery selector of the HTML element that will be clicked/dragger/hovered, etc.
+ * @param {string} event_type_name - The event type, i.e. "click".
+ * @param {Ox.EventListener~methodDelegateParamHandlerCallback} handler - Function that will be called when the event happens.
+ */
 Ox.EventListener.delegate = function(selector, event_type_name, handler){
     // Start delegatable as an empty object
     let delegatable = {};
@@ -23,3 +32,10 @@ Ox.EventListener.delegate = function(selector, event_type_name, handler){
     // Listen for event to happen on given selector
     $(document).on(delegatable, selector );
 }
+
+/**
+ * Function that will be called when the event happens.
+ * @callback Ox.EventListener~methodDelegateParamHandlerCallback
+ * @param {object} element - jQuery element object
+ * @param {event} event - Event that was triggered
+ */
